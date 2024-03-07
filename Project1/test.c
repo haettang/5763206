600e5b8e2f90da60a613e19221a824632dcb59c1
82103 <82103@LAPTOP-7TCORV3F>
2024-03-07 11:28:33 +0900

    github testing

--- Project1/test.c
+++ Project1/test.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
 int main() {
-	int sum = 0;
-	int n = 0;
-	int i;
-	scanf_s("%n", &n);
-	for (i = 0; i < n; i++) {
-		sum = sum + i;
-	}
-	pritnf("%n", sum);
+	printf("Hello World!");
 }
