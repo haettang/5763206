35592274f8e22fa4ee11ae250f94a98931465514
haettang <155634721+haettang@users.noreply.github.com>
2024-03-19 15:01:48 +0900

    03.19 과제2

--- Project1/test.c
+++ Project1/test.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
+#include <time.h>
 
-int primenum(int n) {
-    if(n <= 1) {
-        return 0;
+long long power_iter(long long x, int n) {
+    int i;
+    long long result = 1;
+    for (i = 0; i < n; i++) {
+        result = result * x;
     }
-    for(int i = 2; i * i <= n; i++) {
-        if(n % i == 0) {
-            return 0;
-        }
+    return(result);
+}
+long long power_rec(long long x, int n) {
+    if (n == 0) return 1;
+    else if ((n % 2) == 0) {
+        long long y = power_rec(x, n / 2);
+        return y * y;
     }
-    return 1;
+    else return x * power_rec(x * x, (n - 1) / 2);
 }
 
 int main() {
-    int sum = 0;
+    clock_t start_rec, end_rec, start_iter, end_iter;
+    
+    start_rec = clock();
+    long long result_rec = power_rec(13, 21);
+    end_rec = clock();
+    printf("\nê±¸ë¦° ì‹œê°„ : %f\n", (float)(end_rec - start_rec) / CLOCKS_PER_SEC);
+    printf("ìž¬ê·€ì  ê²°ê³¼: %lld\n", result_rec);
 
-    for(int i = 0; i <= 100; i++) {
-        if(primenum(i) == 1) {
-            sum += i;
-        }
-    }
-
-    printf("0ºÎÅÍ 100±îÁöÀÇ ¼Ò¼öÀÇ ÇÕÀº %dÀÔ´Ï´Ù.\n", sum);
-    return 0;
+    start_iter = clock();
+    long long result_iter = 0;
+    result_iter = power_iter(13, 21);
+    end_iter = clock();
+    printf("\nê±¸ë¦° ì‹œê°„ : %f\n", (float)(end_iter - start_iter) / CLOCKS_PER_SEC);
+    printf("ë°˜ë³µì  ê²°ê³¼ : %lld\n", result_iter);
 }
